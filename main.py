2d08e9bb9c2943d23673b66a174183a6f3f4acd4
LennartGol <golksart@gmail.com>
2023-02-22 08:15:06 +1300

    erster eig vec gefunden

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import numpy as np
 import numpy.random as rd
+from numpy.fft import fft2, ifft2
 #initialize first samples
 number_samples = 100
 gammas = np.zeros(number_samples)
@@ -66,42 +67,56 @@ for i in range(siz):
         L[i,int(neigbours[i,n])] = -1
         L[int(neigbours[i,n]),i] = -1
 
+#create one fourier basis vector
+        F_bas = np.zeros(siz, dtype = 'csingle' )
+        for m in range(siz):
+                       F_bas[m] = np.exp(-2j * np.pi * (m +1) / img_siz)
+
 #create fourier matrix for each pixel
 F = np.zeros((siz,img_siz,img_siz), dtype = 'csingle')
+W = np.zeros((siz,siz), dtype = 'csingle')
 
 n = 0
 for n in range(siz):
 
-        G = np.zeros(siz)
-        G[n] = numb_neig
-        for x in range(numb_neig):
-            G[int(neigbours[n,x])] = -1
-        G = np.reshape(G, (img_siz, img_siz))
+        G_vec = np.zeros(siz)
+        G_vec[n] = numb_neig
 
+        for x in range(numb_neig):
+            G_vec[int(neigbours[n,x])] = -1
+        G = np.reshape(G_vec, (img_siz, img_siz))
+        print(G_vec)
         #FFT = np.zeros((img_siz, img_siz))
 
         for m in range(img_siz):
             for i in range(img_siz):
                 for l in range(img_siz):
                     for k in range(img_siz):
-                        #G[l,k]
-                        F[n,m,i] = F[n,m,i] + 1 * np.exp(-2j * np.pi * (m * l + i * k)/ img_siz) / img_siz
+                        #G[l,k] F[n,m,i]
+
+                        F[n,m,i] = 1 * np.exp(-2j * np.pi * (m * l + i * k) / img_siz) #/ img_siz
+
+        Four_vec = F[n].T.flatten()
+        W[n] = G_vec * Four_vec
+# W = np.ndarray(shape = (siz,siz), dtype = 'csingle')
+# W_trans = np.zeros((siz,siz))
+# for m in range(siz):
+#     for n in range(siz):
+#         W[m,n] =  np.exp( 2j * np.pi * m * n / siz) #/ np.sqrt(siz)
+# W_trans =   W.T.conj() #/ np.sqrt(siz)
 
 
+V = np.zeros((siz,siz), dtype = 'csingle')
+for n in range(siz):
+    V[:,n] = F[n].T.flatten()
 
-W = np.ndarray(shape = (siz,siz), dtype = 'csingle')
-W_trans = np.zeros((siz,siz))
-for m in range(siz):
-    for n in range(siz):
-        W[m,n] =  np.exp( 2j * np.pi * m * n / siz) / np.sqrt(siz)
-
-W_trans =   W.T.conj() / np.sqrt(siz)
+v_0 = np.diag(V) * np.identity(siz)
 
 
-v_0 = F[0].T.flatten().real
+Z = np.matmul(L, np.diag(V)).real
 
-Z = np.matmul(L,v_0).real
+Z2 = np.matmul(L, V[:,3]).real
 
-Z2 = np.matmul(L,W[:,1]).real
-c = W[:,1].real
+eig_val, eig_vec = np.linalg.eig(L)
+
 print('debugggg')
